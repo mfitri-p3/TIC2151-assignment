70f41ce43c089c8f499f72b7919947dfc7b5a5b5
LIM ZI JIE <30426081+LIMZIJIE@users.noreply.github.com>
2019-09-22 22:51:13 +0800

    Add files via upload

---
+++ Nfa.java
@@ -0,0 +1,76 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.*;
+/**
+ *
+ * @author LIM
+ */
+public class Nfa {
+    
+    String state_name;
+    boolean start_state;
+    boolean final_state;
+    private Map<Integer,Nfa> to_transition = new HashMap<Integer,Nfa>();
+    private Map<Character,Nfa> char_to_transition = new HashMap<Character,Nfa>();
+    
+    // set the state name when create a state and set if the state is starting state or final state
+    // if the state is starting state, then start_state is true, final_state is false
+    // if the state is final state, then final_state is true, start_state is false
+    // if the state is neither starting state or final state, then start_state and final_state are false
+    Nfa()
+    {
+        
+    }
+    
+    Nfa(String name, boolean start_state,boolean final_state)
+    {
+        state_name = name;
+        this.start_state = start_state;
+        this.final_state = final_state;
+    }
+    
+    // every state has its own transition to other state through 0 or 1.
+    // set the transition to the state. Example, 0 is to state 1 and 1 is to itself
+    public void set_totransition(int to_int, Nfa to_state)
+    {
+        to_transition.put(to_int, to_state);
+    }
+    
+    public void set_char_totransition(char to_char, Nfa to_state)
+    {
+        char_to_transition.put(to_char, to_state);
+    }
+    
+    // get and retrieve the state 
+    public Nfa get_transition(int to_int)
+    {
+        return (to_transition.get(to_int));
+    }
+    
+    public Nfa get_char_transition(char to_char)
+    {
+        return (char_to_transition.get(to_char));
+    }
+    
+    public String get_statename(Nfa object)
+    {
+        return (object.state_name);
+    }
+    
+    public boolean get_start()
+    {
+        return (start_state);
+    }
+    
+    public boolean get_final()
+    {
+        return (final_state);
+    }
+}
